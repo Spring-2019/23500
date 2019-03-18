55f4323d7775d09c2d4f238efcd3423ef29fad78
Mike Zamansky <zamansky@gmail.com>
2019-03-18 10:51:24 -0400

    finished sorts

--- classcode/sorts/sorts.cpp
+++ classcode/sorts/sorts.cpp
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <sys/time.h>
-
+#include <math.h>
 
 int find_min(std::vector<int>a , int start_index, int stop_index){
   int min_index = start_index;
@@ -134,7 +134,89 @@ std::vector<int> qsort(std::vector<int> list){
   
   
 }
+void qsort2(std::vector<int> &a, int left, int right)
+{
+  int i = left, j = right;
+  int mid = (i+j)/2;
+  int tmp;
+  int pivot;
 
+  if (a[i]>a[mid] && a[i] > a[j])
+    pivot = a[i];
+  else if (a[j]>a[mid] && a[j] > a[i])
+    pivot = a[j];
+  else
+    pivot = a[mid];
+
+  /* partition */
+  while (i <= j) {
+    while (a[i] < pivot)
+      i++;
+    while (a[j] > pivot)
+      j--;
+    if (i <= j) {
+      tmp = a[i];
+      a[i] = a[j];
+      a[j] = tmp;
+      i++;
+      j--;
+      
+    }
+  }  
+  if (left < j)
+      qsort2(a, left, j);
+
+    if (i< right)
+      qsort2(a, i, right);
+  
+  }
+
+
+int get_digit(int n, int digit){
+  int d;
+  
+  
+  n = n / pow(10,digit);
+  d=n%10;
+ 
+  //  std::cout << "n="<< n << "  d="<<d<<"\n";
+  return d;
+  
+}
+
+std::vector<int> countsort(std::vector<int> list, int digit){
+  std::vector<int> output(list) ;
+  int d;
+  
+  int i,counts[10]={0};
+  
+  
+  for (i = 0 ; i < list.size() ; i++){
+    d = get_digit(list[i],digit);
+    counts[d] = counts[d]+1;
+  }
+
+  for (i=1;i<10;i++){
+    counts[i] += counts[i-1];
+  }
+
+  for (i = list.size()-1 ; i >=0 ;i--){
+    int d = get_digit(list[i],digit);
+        
+    output[ counts[d]-1 ]=list[i];
+    counts[d]--;
+  }
+  
+  return output;
+}
+
+
+std::vector<int> rsort(std::vector<int> list){
+  for (int i=0;i<3;i++){
+    list = countsort(list,i);
+  }
+  return list;
+}
 
 extern char *optarg;
 int main(int argc, char **argv)
@@ -142,7 +224,8 @@ int main(int argc, char **argv)
   bool print = false;
   int  c;
   int size = 10;
-  char algorithm ='s'; // m = merge, s = selection, q = quick
+  char algorithm ='s'; // m = merge, s = selection, q = quick, r = radix,
+  // 2 = qsort2
 
   
   while ((c=getopt(argc,argv,"ps:a:h")) != -1){
@@ -171,7 +254,7 @@ int main(int argc, char **argv)
   srand(time(NULL));
   std::vector<int> a(size);
   for (int i = 0; i < size; ++i) {
-    a[i]=rand()%500;
+    a[i]=rand()%1000;
   }
   if (print)
     print_vector(a);
@@ -186,7 +269,11 @@ int main(int argc, char **argv)
     a = msort(a);
   else if(algorithm == 'q')
     a = qsort(a);
-
+  else if(algorithm == '2')
+    qsort2(a,0,a.size()-1);
+  else if (algorithm =='r'){
+    a = rsort(a);
+  }
   gettimeofday(&tp,NULL);
   long current_time = tp.tv_sec * 1000 + tp.tv_usec / 1000;
   long elapsed = current_time - start_time;
